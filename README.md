1a1738fdc19f240cbc30d50c3444e501269e6d6e
Erica Lauer Vose <elauervose@gmail.com>
2017-06-12 17:38:45 -0700

    add the admin tab for adding new clients

--- README.md
+++ README.md
@@ -10,7 +10,6 @@ Rules for client logos
   3. image size = 3 inches wide x 1 inch tall (set largest dimension to the limit and then enlarge the canvas size to meet size requirements)
  -->
 
- <!-- Image thumbs for featured posts should be 4 inches wide x 3 inches high -->
 
  Note to myself:
  * remember to change the URL for the pinterest "share button" once the site goes live
---
+++ js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+  "use strict";
+
+  // Copy to clipboard script
+  // ===============================
+  $("#copyButton").on("click", function(event) {
+
+    // These two things will likely be needed if you want to use classes
+    // for the copyToClipboard function.
+    var buttonPressed = event.target;
+    var copyTarget = $(buttonPressed).data("target");
+
+    // Since copyToClipboard is Vanilla.js, we need to convert from jQuery syntax
+    // to Vanilla by adding [0] after the $().
+    copyToClipboard($("#copyTarget")[0]);
+  });
+});
+
