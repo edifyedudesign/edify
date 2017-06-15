04ccf9521dd8144ec43aeda59c10a3b1fadd2ab2
Erica Lauer Vose <elauervose@gmail.com>
2017-06-14 19:13:33 -0700

    add admin password to page

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,38 @@ $(document).ready(function() {
     // to Vanilla by adding [0] after the $().
     copyToClipboard($("#copyTarget")[0]);
   });
-});
 
+
+  // NOTE: This is not meant to be secure!
+  var password = "learningisfun!";
+
+  // To clear your localStorage for testing: `localStorage.setItem("passwordMatched", "")`
+  // To check your localStorage: `localStorage.getItem("passwordMatched")`
+  // Or in the chrome settings, clear out "Cookies and other site and plugin data"
+
+  // Automatically show the existing client views if they've entered the password before
+  if (localStorage.getItem('passwordMatched') === "true") {
+    // $("#passwordSubmitWrapper").addClass("hide");
+    $("#admin-access").addClass("hide");
+    $("#admin-page").removeClass("hide");
+  }
+
+  // Enable the password dialog
+  $("#passwordSubmit").on("submit", function (e) {
+    e.preventDefault();
+
+    // If the password matches, show the existing client views
+    if ($("input[type=password]", e.target).val() == password) {
+      $("#passwordSubmitWrapper").addClass("hide");
+      $("#admin-access").addClass("hide");
+      $("#admin-page").removeClass("hide");
+
+      // In the future, don't show the password prompt, just let them in.
+      localStorage.setItem('passwordMatched', true);
+    }
+
+    return false;
+  });
+
+});
 
