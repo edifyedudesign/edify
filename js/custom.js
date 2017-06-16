2e40370613463cd14ea64d9dbeed6a48d355d11e
Chuck Lauer Vose <vosechu@gmail.com>
2017-06-15 19:14:09 -0700

    Add click events for GA

--- js/custom.js
+++ js/custom.js
@@ -14,6 +14,28 @@ $(document).ready(function() {
     // to Vanilla by adding [0] after the $().
     copyToClipboard($("#copyTarget")[0]);
   });
+
+  // Google analytics event tracking
+  //https://developers.google.com/analytics/devguides/collection/analyticsjs/events
+
+  // Actions to track:
+  // ===================
+  //   1. clicked to flip card
+  //   2. clicked "learn more" (or "contact me") button
+
+  $('.card .front.face').on('click', function(event) {
+    var cardName = $(event.currentTarget).data("cardnamefront")
+
+    // Sends the event to Google Analytics and
+    // resubmits the form once the hit is done.
+    ga('send', 'event', 'Flip Card', 'front', cardName);
+  });
+
+  $('.about-button a').on('click', function(event) {
+    var cardName = $(event.currentTarget).data("cardnamebutton")
+
+    ga('send', 'event', 'Flip Card', 'back', cardName);
+  });
 });
 
 
